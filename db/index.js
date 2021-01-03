c7f59969b7a5373bc6b851e4b013ec31ac4719e3
timvvoodman <71102666+timvvoodman@users.noreply.github.com>
2021-01-03 13:22:44 -0500

    comments updated and all functions test good in CLA

--- db/index.js
+++ db/index.js
@@ -1,19 +1,19 @@
 const connection = require("./connection");
 
+//Database opertations class
 class DB {
-  // Keeping a reference to the connection on the class in case we need it later
   constructor(connection) {
     this.connection = connection;
   }
 
-  // Find all employees, join with roles and departments to display their roles, salaries, departments, and managers
+  // Find all Employees Functionality: Select employees, join with roles and departments to display their roles, salaries, departments, and managers
   findAllEmployees() {
     return this.connection.query(
       "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
     );
   }
 
-  // Find all employees except the given employee id
+  // Find Manager for Employees Functionality: Find all employees except the given employee id
   findAllPossibleManagers(employeeId) {
     return this.connection.query(
       "SELECT id, first_name, last_name FROM employee WHERE id != ?",
@@ -21,12 +21,12 @@ class DB {
     );
   }
 
-  // Create a new employee
+  // Create Functionality: Create new employee
   createEmployee(employee) {
     return this.connection.query("INSERT INTO employee SET ?", employee);
   }
 
-  // Remove an employee with the given id
+  //Delete Functionality: Remove an employee with the given id
   removeEmployee(employeeId) {
     return this.connection.query(
       "DELETE FROM employee WHERE id = ?",
@@ -34,7 +34,7 @@ class DB {
     );
   }
 
-  // Update the given employee's role
+  // Update Functionality: changes the given employee's role
   updateEmployeeRole(employeeId, roleId) {
     return this.connection.query(
       "UPDATE employee SET role_id = ? WHERE id = ?",
@@ -42,7 +42,7 @@ class DB {
     );
   }
 
-  // Update the given employee's manager
+  // Update Manager: updates the given employee's manager
   updateEmployeeManager(employeeId, managerId) {
     return this.connection.query(
       "UPDATE employee SET manager_id = ? WHERE id = ?",
